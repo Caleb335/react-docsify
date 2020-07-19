9e42ccaf8a5f2c2b8910ed9ee51e7208080ff7c9
Caleb335 <belac335@gmail.com>
2020-07-19 10:42:46 +0100

    comaptibility with react isFalse for now

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ReactDOM from "react-dom";
+import App from './components/app'
+import docs from './components/docs/guide.md'
 
 import "./css/app.css";
 
-export default function Welcome() {
-  return (
-    <div className="app">
-      <h1>Testing React With Docsify</h1>
-    </div>
-  );
-}
+<Router>
+  <Switch>
+    <Route exact path="/" component={App}/>
+    <Route exact path="docs" component={docs} />
+  </Switch>
+</Router>
 
-ReactDOM.render(<Welcome />, document.querySelector("#app"));
+ReactDOM.render(<App />, document.querySelector("#app"));
